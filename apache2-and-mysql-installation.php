85c5ec67a55dd558ef2aee2a23108019466a2296
incredible <jitti327@gmail.com>
2018-10-18 13:08:42 +0530

    How to install apache2 and mysql server in ubuntu

---
+++ apache2-and-mysql-installation.php
@@ -0,0 +1,21 @@
+
+How to install apache2 and mysql server in ubuntu
+
+
+How to install apache2 in ubuntu:- 
+
+1) Installing apache2
+
+Ref: https://howtoubuntu.org/how-to-install-lamp-onubuntu
+
+a) sudo apt install apache2
+
+
+
+How to install mysql in ubuntu:- 
+
+1) Installing apache2
+
+Ref: https://howtoubuntu.org/how-to-install-lamp-onubuntu
+
+a) sudo apt insatll mysql-server
