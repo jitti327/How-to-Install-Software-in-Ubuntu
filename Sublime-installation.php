b038c53aad6bbbd4bc557491408f6e53b55b49a8
incredible <jitti327@gmail.com>
2018-10-18 13:22:29 +0530

    How to install Sublime in ubuntu:-

---
+++ Sublime-installation.php
@@ -0,0 +1,10 @@
+
+How to install Sublime in ubuntu:-
+
+1) Installing Sublime:- 
+
+Ref: https://linuxconfig.org/how-to-install-sublime-text-on-ubuntu-18-04-biomic-beaver-linux
+
+a) $ wget-q0-https://download.sublimetext.com/sublimehq-pub.gpg |sudo apt-key add
+b) $sudo apt-add-repository "deb https://download.sublimetext.com/apt/stable/"
+c) $sudo apt install sublime-text
