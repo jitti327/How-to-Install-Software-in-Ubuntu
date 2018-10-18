148cc7334de5507a1fa2ca95924e229a7657b07d
incredible <jitti327@gmail.com>
2018-10-18 13:32:25 +0530

    How to install Git in ubuntu:-

---
+++ Git-installation.php
@@ -0,0 +1,19 @@
+
+How to install Git in ubuntu:-
+
+1) Installing Git:- 
+
+a) $ sudo apt-get-update
+b) $ sudo apt-get Install git-core
+
+
+
+
+Then You can easily work on Git like:- (Please press Enter After all commands you used)
+
+A) git clone:- for making clone of repository
+B) git status:- status of file
+C) git add "Enter file name here"
+D) git commit -a 
+E) git pull: git pull is used to pull update file from your GitHub account when You used your data in diffenet			computer to edit your file
+F) git push: to push your files in github account
